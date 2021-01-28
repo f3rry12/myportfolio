29a85daf5776c23d202a77dab90f374f02356eea
f3rry12 <f3rry12@gmail.com>
2021-01-28 13:05:13 +0700

    Initial commit, embed Aframe disabled

---
+++ js/home.js
@@ -0,0 +1,13 @@
+
+
+// // navigasi bar
+// $(document).ready(function() {
+//   $(window).scroll(function() {
+//     if($(this).scrollTop() < $("#myEmbeddedScene").height()){
+//        $(".navbar").removeClass("bg-dark");
+//     }
+//     else{
+//        $(".navbar").addClass("bg-dark");
+//     }
+//   });
+// });
