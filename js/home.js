2421030725cd6870664287d2670c961743824807
f3rry12 <f3rry12@gmail.com>
2021-04-07 12:33:45 +0700

    mark2: whaticando bigchanges

--- js/home.js
+++ js/home.js
@@ -15,11 +15,11 @@ $(window).scroll(function() {
     }
 });
   
-  
-  
-  
-
-  
+$('#ilusWebVr').hover(
+  function(){$(this).attr("src","https://cdn.glitch.com/0f211e7c-212a-4d51-a2bd-e9caa1d12445%2Fvrborobudur.png?v=1617765327839")},
+  function(){$(this).attr("src","https://cdn.glitch.com/0f211e7c-212a-4d51-a2bd-e9caa1d12445%2Fwebborobudur.png?v=1617765330985")}
+);
 // document ready  
 });
+
 
